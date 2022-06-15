07641c54633df0c78bec420dcc50ec60aaf2ee6c
Anamol Adhikari <anamoladhikari1998@gmail.com>
2022-06-15 15:13:50 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Anamol Adhikari on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
